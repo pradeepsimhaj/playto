ae759e9881241a00dc1876d465b672502fab1ea7
jbkpsr <jbkpsr1999@gmail.com.com>
2026-04-28 12:08:08 +0530

    cors allowed origins updated

--- playto-backend/payout_engine/settings.py
+++ playto-backend/payout_engine/settings.py
@@ -77,6 +77,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     # os.getenv("Backend_Url"),
     'http://localhost:5743',
+    'https://playto-two.vercel.app/payouts'
 ]
 
 ROOT_URLCONF = 'payout_engine.urls'
