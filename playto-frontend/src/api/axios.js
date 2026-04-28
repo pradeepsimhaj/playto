02d27378afe5e7a67452a0bef267a67dd2468036
jbkpsr <jbkpsr1999@gmail.com.com>
2026-04-28 12:02:47 +0530

    setup frontend vercel proxy

--- playto-frontend/src/api/axios.js
+++ playto-frontend/src/api/axios.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
-// const backendUrl = "http://127.0.0.1:8000";
-
-
 const API = axios.create({
-  baseURL: backendUrl + "/api/v1",
+  baseURL: "/api/v1",
 });
 
 export default API;
